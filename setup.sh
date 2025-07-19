529db30f59d199025433e8947af4c63686346ec6
Nathan <57642318+nathnp@users.noreply.github.com>
2025-07-19 00:26:32 -0400

    Added script stuff

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update
+
+apt install fish tmux duf ncdu curl wget rsync -y
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+{
+	echo "#!/bin/bash"
+	echo "tailscale up --qr"
+	echo "rm ts-setup.sh"
+} >> /root/ts-setup.sh
+
+chmod +x ts-setup.sh
+
+apt upgrade -y
+
+reboot
