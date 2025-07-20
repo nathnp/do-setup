3e255f7fb037ac3c2c6cb6b89fbb250fe3bb8fda
Nathan <57642318+nathnp@users.noreply.github.com>
2025-07-19 23:54:29 -0400

    Added btop

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt update
 
 apt upgrade -y
 
-apt install fish tmux duf ncdu curl wget rsync -y
+apt install fish tmux btop duf ncdu curl wget rsync -y
 
 apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
